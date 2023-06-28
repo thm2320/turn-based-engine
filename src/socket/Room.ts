0a09bb33484d04225d685549be3fbbd76550fee7
Ming Tsang <tghomg@gmail.com>
2023-06-28 10:57:10 +0100

    handle disconnect; cleanup room and game

--- src/socket/Room.ts
+++ src/socket/Room.ts
@@ -1,50 +1,64 @@
 import { TurnBasedGame } from "../games/TurnBasedGame";
+import { Server } from 'socket.io';
 import { Player } from "./Player";
 
 export class Room {
+  io: Server;
   name: string;
   players: Player[];
   limit: number;
   game: TurnBasedGame;
 
-  constructor(name: string, limit: number, game: TurnBasedGame) {
+  constructor(io: Server, name: string, limit: number, game: TurnBasedGame) {
+    this.io = io;
     this.name = name;
     this.limit = limit;
     this.players = [];
     this.game = game;
   }
 
+  cleanUp = () : void => {
+    this.game.cleanUp();
+    this.players = [];
+  }
+
   addPlayer = (player: Player): void => {
-    if (this.isFull()){
+    if (this.isFull()) {
       throw new Error('Room is full');
     }
+    player.socket.join(this.name);
     this.players.push(player);
-    this.game.addPlayer(player)
-    if (this.game.canStart()){
+    this.game.addPlayer(player);
+    if (this.game.canStart()) {
       this.setUpListeners();
       this.game.status = 'playing';
     }
     return;
-  }
+  };
 
   isFull = () => {
-    return this.players.length >= this.limit
+    return this.players.length >= this.limit;
+  };
+
+  sendMessage = (msg: string) => {
+    console.log(`room sends msg ${msg}`)
+    this.io.in(this.name).emit('message', msg);
   }
 
   setUpListeners = () => {
-    this.players.forEach( (player ) => {
+    this.players.forEach((player) => {
       player.socket.on('play', (steps) => {
         console.log(`${player.socket.id} run ${steps}`);
-        try{
-          this.game.play(player, steps)
-          if (this.game.status === 'finished'){
-            console.log(`${player.socket.id} won!`)
+        try {
+          this.game.play(player, steps);
+          if (this.game.status === 'finished') {
+            console.log(`${player.socket.id} won!`);
           }
-        }
-        catch(e: any) {
+          this.io.in(this.name).emit('update', {player: player.socket.id, step: steps})
+        } catch (e: any) {
           console.log(e);
         }
       });
-    })
-  }
+    });
+  };
 }
--- src/socket/SocketServer.ts
+++ src/socket/SocketServer.ts
@@ -6,7 +6,7 @@ import { Player } from './Player';
 
 export class SocketServer {
   private io: Server;
-  private rooms: Room[] = [];
+  private rooms: Map<string, Room> = new Map();
 
   constructor(httpServer: HttpServer) {
     this.io = new Server(httpServer,{
@@ -19,29 +19,49 @@ export class SocketServer {
 
   init(): void {
     this.io.on('connection', (socket) => {
-      const newPlayer = new Player(socket);
-
       console.log(`${socket.id} connected`);
-      socket.emit('hi', `hi ${socket.id}`);
+      
+      const newPlayer = new Player(socket);
       const room = this.getAvailableRoom();
       room.addPlayer(newPlayer);
-      socket.join(room.name); 
-      this.io.in(room.name).emit('hi', `${socket.id} joined`);
+      room.sendMessage(`${socket.id} joined`);
+      console.log(this.io.of('/').adapter.rooms)
+
+      socket.on('disconnecting', () => {
+        console.log(`${socket.id} disconnecting ======`)
+        const socketRoom = this.io.of('/').adapter.rooms.get(room.name);
+        console.log(`socketRoom ${socketRoom}`)
+        console.log(`Rooms size: ${this.rooms.size}`)
+      })
+      socket.on('disconnect', () => {
+        console.log(`${socket.id} disconnected ======`)
+
+        const socketRoom = this.io.of('/').adapter.rooms.get(room.name);
+        if (!socketRoom) {
+          console.log('socketRoom not found')
+          room.cleanUp();
+          this.rooms.delete(room.name)
+        }
+        console.log(`Rooms size: ${this.rooms.size}`)
+        console.log(socket.rooms)
+      })
     });
   }
 
   getAvailableRoom = () => {
-    for (const room of this.rooms) {
+    for (const [, room] of this.rooms) {
       if (!room.isFull()) {
         return room;
       }
     }
+    const roomName = `FourChainChessRoom-${new Date().valueOf()}`;
     const newRoom = new Room(
-      `FourChainChessRoom-${this.rooms.length}`,
+      this.io,
+      roomName,
       2,
       new FourChainChess()
     );
-    this.rooms.push(newRoom);
+    this.rooms.set(roomName, newRoom)
     return newRoom;
   };
 }
