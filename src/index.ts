3730a65acb0726a03319dbe1679b3a90232cedcc
Ming Tsang <tghomg@gmail.com>
2023-08-04 13:53:17 +0100

    url for testing deployment

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import express from "express";
-import { createServer, Server as HttpServer } from "http";
+import express, { Request, Response } from 'express';
+import { createServer, Server as HttpServer } from 'http';
 // import { roomRouter } from "./routes/rooms";
-import { json } from "body-parser";
-import { SocketManager } from "./socket/SocketManager";
-import socketServer from "./socket/socketServer";
+import { json } from 'body-parser';
+import { SocketManager } from './socket/SocketManager';
+import socketServer from './socket/socketServer';
 
 const app = express();
 app.use(json());
 // app.use(roomRouter);
+
+app.get('/', (_req: Request, res: Response) => {
+  return res.send('Express Typescript on Vercel');
+});
 
 const httpServer: HttpServer = createServer(app);
 const io = socketServer(httpServer);
@@ -15,5 +19,5 @@ export const socketManager = new SocketManager(io);
 socketManager.init();
 
 httpServer.listen(3000, () => {
-  console.log('listening on port 3000')
+  console.log('listening on port 3000');
 });
